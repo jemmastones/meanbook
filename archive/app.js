ef0827b2fdd07445b307d0d321e917494f35523d
Jemma Stones <jemma.stones@servian.com.au>
2017-08-29 22:32:19 +1000

    Initial checkin - 10.32pm @ Tues evening

---
+++ archive/app.js
@@ -0,0 +1,7 @@
+var http = require("http");
+http.createServer(function(request, response) { 
+	console.log("response outgoing")
+	response.writeHead(200, {"Content-Type":"text/plain"});
+	response.write("Hello World");
+	response.end();
+}).listen(3000);
