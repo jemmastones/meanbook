f1c3c884f67d675d4de7b2ff6ba5f97d24e0d879
Jemma Stones <jemmastones@SVN-15-074-Jemma.local>
2017-09-28 15:11:37 +0100

    end of chapter 5

--- archive/app.js
+++
@@ -1,7 +0,0 @@
-var http = require("http");
-http.createServer(function(request, response) { 
-	console.log("response outgoing")
-	response.writeHead(200, {"Content-Type":"text/plain"});
-	response.write("Hello World");
-	response.end();
-}).listen(3000);
--- assets/app.js
+++ assets/app.js
@@ -1,45 +1,27 @@
-  var app = angular.module('app', [])
+var app = angular.module('app', [])
 
-  app.service('PostsSvc', function($http) {
-  	this.fetch = function() {
-  		return $http.get('/api/posts')
-  	}
-  	this.create = function (post) {
-    	return $http.post('/api/posts', post)
+app.service('PostsSvc', function($http) {
+		this.fetch = function() {
+			return $http.get('/api/posts')
+		}
+		this.create = function (post) {
+		return $http.post('/api/posts', post)
 	}
-  })
+	})
 
-/*
-  app.controller('PostsCtrl', function($scope, PostsSvc) {
-
-      $scope.addPost = function() {
-        // Only add a post if there is a body
-        if ($scope.postBody) {
-            // unshift a new post into $scope.posts
-            $http.post('/api/posts', {
-              username: 'jemmastones',
-              body:  $scope.postBody
-            }).success(function(post) {
-              $scope.posts.unshift(post)
-              $scope.postBody = null
-            })
-        }
-    }
-    */
-
-    app.controller('PostsCtrl', function ($scope, PostsSvc) {
-  $scope.addPost = function () {
-    if ($scope.postBody) {
-      PostsSvc.create({
-        username: 'jemmastones',
-        body: $scope.postBody
-      }).success(function (post) {
-        $scope.posts.unshift(post)
-        $scope.postBody = null
-      })
-    }
-  }
- PostsSvc.fetch().success(function (posts) {
-    $scope.posts = posts
-  })
+app.controller('PostsCtrl', function ($scope, PostsSvc) {
+  	$scope.addPost = function () {
+    	if ($scope.postBody) {
+      		PostsSvc.create({
+        		username: 'jemmastones',
+        		body: $scope.postBody
+      		}).success(function (post) {
+        		$scope.posts.unshift(post)
+        		$scope.postBody = null
+      		})
+    	}
+  	}
+ 	PostsSvc.fetch().success(function (posts) {
+    	$scope.posts = posts
+  	})
 })
