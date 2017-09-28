7ad3338ee861095c0cffb449edf7b7a4ba94b384
Jemma Stones <jemmastones@SVN-15-074-Jemma.local>
2017-09-28 15:04:17 +0100

    train to london 2

--- assets/app.js
+++ assets/app.js
@@ -9,6 +9,7 @@
 	}
   })
 
+/*
   app.controller('PostsCtrl', function($scope, PostsSvc) {
 
       $scope.addPost = function() {
@@ -24,16 +25,21 @@
             })
         }
     }
+    */
 
-/*
-    $http.get('http://localhost:3000/api/posts')
-    .success(function (posts) {
-      $scope.posts = posts
-    })
-*/
-
-	PostsSvc.fetch().success(function(posts) {
-		$scope.posts = posts
-	})
-
+    app.controller('PostsCtrl', function ($scope, PostsSvc) {
+  $scope.addPost = function () {
+    if ($scope.postBody) {
+      PostsSvc.create({
+        username: 'jemmastones',
+        body: $scope.postBody
+      }).success(function (post) {
+        $scope.posts.unshift(post)
+        $scope.postBody = null
+      })
+    }
+  }
+ PostsSvc.fetch().success(function (posts) {
+    $scope.posts = posts
   })
+})
